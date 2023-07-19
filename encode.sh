b2b1af709f3dc4ef70fa41ef3f6afd438e46cd60
Ruben Martinez <ruben.martinez@io.builders>
2023-07-19 21:52:25 +0200

    another encode

--- encode.sh
+++ encode.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#ffmpeg -i $1 -c:v libx264 -c:a aac -crf 23 -vf format=yuv420p -b:v 3500k -b:a 128k -framerate 30 $2
-ffmpeg -i $1 -c:v libx264 -b:v 3500k -vf format=yuv420p -c:a aac -b:a 128k $2
+ffmpeg -i $1 -pix_fmt yuv420p -c:v libx264 -b:v 3500k -c:a aac -movflags +faststart $2
+
